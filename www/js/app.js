ced7b9594fdbf50b7c11467879d7cc9130317ac3
Atsushi <atsushi@moongift.jp>
2022-08-03 15:40:35 +0900

    Update

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,16 @@
 const $ = Dom7;
 
-// Login Screen Demo
-$('#my-login-screen .login-button').on('click', function () {
-  var username = $('#my-login-screen [name="username"]').val();
-  var password = $('#my-login-screen [name="password"]').val();
-
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
-
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br/>Password: ' + password);
-});
-
+let webSocket;
 // NCMBの初期化用
 const event = window.cordova ? 'deviceready' : 'DOMContentLoaded';
 document.addEventListener(event, async (e) => {
   const config = await (await fetch('./js/config.json')).json();
   window.ncmb = new NCMB(config.applicationKey, config.clientKey);
+  const webSocket = new WebSocket(config.websocketUrl);
+  webSocket.onopen = onOpen;
+  webSocket.onclose = onClose;
+  webSocket.onerror = onError;
+  window.webSocket = webSocket;
   window.app = new Framework7({
     name: 'My App', // App name
     theme: 'auto', // Automatic theme detection
@@ -27,3 +21,13 @@ document.addEventListener(event, async (e) => {
     routes: routes,
   });
 });
+
+const onOpen = (e) => {
+}
+
+const onError  = (e) => {
+  console.log(e);
+};
+
+const onClose = (e) => {
+}
--- www/js/routes.js
+++ www/js/routes.js
@@ -3,77 +3,29 @@ const routes = [
     path: '/',
     url: './index.html',
   },
+  // チャット画面
   {
-    path: '/about/',
-    url: './pages/about.html',
+    path: '/chat/',
+    componentUrl: './pages/chat.html',
+    beforeEnter: ({ resolve, reject, router }) => {
+      // ログインユーザー情報の取得
+      const user = ncmb.User.getCurrentUser();
+      if (!user) {
+        // 認証していない場合（user === null）
+        // エラー判定
+        reject();
+        // ログイン画面に遷移
+        router.navigate('/login/');
+      } else {
+        // 認証している場合
+        resolve();
+      }
+    }
   },
+  // ログイン画面
   {
-    path: '/form/',
-    url: './pages/form.html',
-  },
-  {
-    path: '/catalog/',
-    componentUrl: './pages/catalog.html',
-  },
-  {
-    path: '/product/:id/',
-    componentUrl: './pages/product.html',
-  },
-  {
-    path: '/settings/',
-    url: './pages/settings.html',
-  },
-
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    componentUrl: './pages/dynamic-route.html',
-  },
-  {
-    path: '/request-and-load/user/:userId/',
-    async: function ({ router, to, resolve }) {
-      // App instance
-      var app = router.app;
-
-      // Show Preloader
-      app.preloader.show();
-
-      // User ID from request
-      var userId = to.params.userId;
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            componentUrl: './pages/request-and-load.html',
-          },
-          {
-            props: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
-    },
+    path: '/login/',
+    componentUrl: './pages/login.html',
   },
   // Default route (404 page). MUST BE THE LAST
   {
